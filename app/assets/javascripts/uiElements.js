ebba05ba078a5d1768557b9693018f5db0d132a7
Neil <neilspurgeon@gmail.com>
2015-07-16 11:07:40 -0700

    navbar mobile

---
+++ app/assets/javascripts/uiElements.js
@@ -0,0 +1,42 @@
+  $(function() {
+  // Hamburger Icon
+  // var menuBttn = $("#menu-bttn");
+  // alert();
+  // // set on click event
+  // menuBttn.on("click", function(e) {
+  //   alert();
+  //   // select the lines with Snap
+  //   var menu = Snap("#menu-svg"),
+  //     lineOne   = menu.select("#line-one"),
+  //     lineTwo   = menu.select("#line-two"),
+  //     lineThree = menu.select("#line-three");
+    
+  //   menuBttn.toggleClass("close").promise().done(function() {
+  //    if (menuBttn.hasClass("close") === false) {
+
+  //       // move lines to center
+  //       lineOne.animate({"y1":"50", "y2":"50"}, 100);
+  //       lineThree.animate({"y1":"50", "y2":"50"}, 100, function() {
+
+  //         // wait for lines to move to center
+  //         $("#line-two").hide(); // hide center line
+  //         // rotate lines
+  //         lineOne.animate({transform: "r45, 50, 50"}, 100, mina.ease);
+  //         lineThree.animate({transform: "r-45, 50, 50"}, 100, mina.ease) ;     
+  //       });  
+  //     } else {
+
+  //       // move lines back to og position
+  //       lineOne.animate({transform: "0, 50, 50"}, 100);
+  //       lineThree.animate({transform: "0, 50, 50"}, 100, function() {
+
+  //         // wait for lines to move to og position
+  //         $("#line-two").show(); // show center line
+  //         // rotate lines
+  //         lineOne.animate({"y1": "5", "y2": "5" }, 100, mina.ease);
+  //         lineThree.animate({"y1":"95", "y2":"95"}, 100, mina.ease);  
+  //       });   
+  //     }
+  //   });    
+  // });
+});
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'breakpoint'
 require 'susy'
 require "rails"
 # Pick the frameworks you want:
